2f864644233b130502d9eb0500363c9c37948f57
Killua7 <iyadtout@gmail.com>
2020-06-05 13:14:36 +0200

    l

--- Mains/Main.cpp
+++ Mains/Main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
 #include "../GeoMap/GeoMap.h"
-
+#include "../LectureDonnes/FluxLog.h"
+#include "../Grandeur/Grandeur.h"
 using namespace std;
 
 int main(void) {
-	GeoMap_TEST gmt;
-	gmt.run();
+	//GeoMap_TEST gmt;
+	//gmt.run();
+	Grandeur tabgrande[10];
+	FluxLog flux = FluxLog("donne.csv");
+	int i=0;
+	
+	while(!flux.eof() && i<10){
+		flux.lireDonneesGrandeur(tabgrande[i]);
+		cout << "ca passe"<<endl;
+		i++;
+	}
+	
 }
