d08500c6d6be39fbfccf0df9c819a3abca14f9b8
sdejoannis <62374800+sdejoannis@users.noreply.github.com>
2020-06-05 13:35:11 +0200

    Add files via upload

---
+++ Grandeur/Grandeur.cpp
@@ -0,0 +1,82 @@
+/*************************************************************************
+Grandeur:
+DESCRIPTIF : L'objet Grandeur contiendra l'ensemble des trajets simples ou composés.
+Il sera instancié dans le main. L'utilisateur peut y ajouter autant de trajet qu'il veut.
+Les trajets sont contenus dans une liste chainée de trajets (cf. struct listeTrajet dans Grandeur.h).
+
+*************************************************************************/
+
+//----- Réalisation de la classe <Grandeur> (fichier Grandeur.cpp) -----
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+#include <string>
+//------------------------------------------------------ Include personnel
+#include "Grandeur.h" 
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+void Grandeur::Afficher() const
+{
+    cout << "Identifiant : " << Identifiant << " - Description : " << Description << " - Unité de mesure : " << Unite << endl;
+}
+
+void Grandeur::SetIdentifiant( const string identifiant ) {
+    Identifiant = identifiant;
+}
+
+void Grandeur::SetDescription( const string description ) {
+    Description = description;
+}
+
+void Grandeur::SetUnite( const string unite ) {
+    Unite = unite;
+}
+
+string Grandeur::GetIdentifiant() {
+    cout << "Identifiant : " << Identifiant << endl;
+    return Identifiant;
+}
+
+string Grandeur::GetDescription() {
+    return Description;
+}
+
+string Grandeur::GetUnite() {
+    return Unite;
+}
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Grandeur::Grandeur ( string identifiant, string description, string unite )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Grandeur>" << endl;
+#endif
+Identifiant = identifiant;
+Description = description;
+Unite = unite;
+}
+
+
+Grandeur::~Grandeur ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Grandeur>" << endl;
+#endif
+}
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+
+
+
---
+++ Grandeur/Grandeur.h
@@ -0,0 +1,53 @@
+/*************************************************************************
+Grandeur:
+DESCRIPTIF : L'objet Grandeur contiendra l'ensemble des trajets simples ou composés.
+Il sera instancié dans le main. L'utilisateur peut y ajouter autant de trajet qu'il veut.
+Les trajets sont contenus dans une liste chainée de trajets (cf. struct listeTrajet dans Grandeur.h).
+
+*************************************************************************/
+using namespace std;
+#include <ctime>
+#include <string>
+
+//---------- Interface de la classe <Grandeur> (fichier Grandeur.h) ----------------
+#if ! defined ( Grandeur_H )
+#define Grandeur_H
+
+//--------------------------------------------------- Interfaces utilisées
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+//------------------------------------------------------------------------
+
+class Grandeur
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+
+//----------------------------------------------------- Méthodes publiques
+    void Afficher() const;
+    string GetIdentifiant(), GetDescription(), GetUnite();
+    void SetIdentifiant( string identifiant ), SetDescription( string description ), SetUnite( string unite );
+
+//-------------------------------------------- Constructeurs - destructeur
+    Grandeur ( string Identifiant = "", string Description = "", string Unite = "");
+    ~Grandeur ( );
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+string Identifiant;
+string Description;
+string Unite;
+
+friend class FluxLog; 
+};
+
+//-------------------------------- Autres définitions dépendantes de <Grandeur>
+
+#endif // Grandeur_H
+
