4d42a9498251c66c46d41e5bd28243151d2fdcfc
ericu07 <ericu07@users.noreply.github.com>
2020-06-05 15:12:49 +0200

    Code coordonnees dans .cpp

--- Coordonnees/Coordonnees.cpp
+++ Coordonnees/Coordonnees.cpp
@@ -1,1 +1,39 @@
 #include "Coordonnees.h"
+
+double Coordonnees::distanceTo(const Coordonnees& coord) const {
+	double* pointA = toXYZ();
+	double* pointB = coord.toXYZ();
+
+	pointA[0] -= pointB[0];
+	pointA[1] -= pointB[1];
+	pointA[2] -= pointB[2];
+
+	delete[] pointB;
+
+	double squaredDistance = pointA[0] * pointA[0] + pointA[1] * pointA[1] + pointA[2] * pointA[2];
+
+	delete[] pointA;
+
+	return sqrt(squaredDistance);
+}
+
+double* Coordonnees::toXYZ() const {
+	/*
+			 x
+			 |
+			 |_____ z
+			 /
+			/
+		   y
+	*/
+
+	const double longitueRadian = longitude * M_PI / 180.0;
+	const double latitudeRadian = latitude * M_PI / 180.0;
+	const double cosLatitudeRadius = cos(latitudeRadian) * radius;
+
+	double* xyz = new double[3];
+	xyz[0] = sin(latitudeRadian) * radius;
+	xyz[1] = cos(longitueRadian) * cosLatitudeRadius;
+	xyz[2] = sin(longitueRadian) * cosLatitudeRadius;
+	return xyz;
+}
--- Coordonnees/Coordonnees.h
+++ Coordonnees/Coordonnees.h
@@ -37,22 +37,7 @@ class Coordonnees
 		inline double getRadius() const { return radius; }
 
 		// distance non géodésique, en metres
-		double distanceTo(const Coordonnees& coord) const {
-			double* pointA = toXYZ();
-			double* pointB = coord.toXYZ();
-
-			pointA[0] -= pointB[0];
-			pointA[1] -= pointB[1];
-			pointA[2] -= pointB[2];
-
-			delete[] pointB;
-
-			double squaredDistance = pointA[0] * pointA[0] + pointA[1] * pointA[1] + pointA[2] * pointA[2];
-
-			delete[] pointA;
-
-			return sqrt(squaredDistance);
-		}
+		double distanceTo(const Coordonnees& coord) const;
 		static inline double distance(const Coordonnees& coordA, const Coordonnees& coordB) {
 			return coordA.distanceTo(coordB);
 		}
@@ -62,26 +47,8 @@ class Coordonnees
 		double latitude;
 		double radius;
 
-		double* toXYZ() const {
-			/*
-			         x
-					 |
-			         |_____ z
-					 /
-					/
-				   y
-			*/
+		double* toXYZ() const;
 
-			const double longitueRadian = longitude * M_PI / 180.0;
-			const double latitudeRadian = latitude * M_PI / 180.0;
-			const double cosLatitudeRadius = cos(latitudeRadian) * radius;
-
-			double* xyz = new double[3];
-			xyz[0] = sin(latitudeRadian) * radius;
-			xyz[1] = cos(longitueRadian) * cosLatitudeRadius;
-			xyz[2] = sin(longitueRadian) * cosLatitudeRadius;
-			return xyz;
-		}
 	friend class FluxLog; 
 };
 
