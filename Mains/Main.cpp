65db3d640889be52a501db6abd9bcdf13b8b310c
Alex <bremard.alexandre@gmail.com>
2020-06-05 14:12:08 +0200

    main avec test Alexandre

--- Mains/Main.cpp
+++ Mains/Main.cpp
@@ -5,11 +5,13 @@
 #include "../Grandeur/Grandeur.h"
 #include "../Mesure/Mesure.h"
 #include "../Capteur/Capteur.h"
+#include "../Qualite_Air/Qualite.h"
 using namespace std;
 
 int main(void) {
 	//GeoMap_TEST gmt;
 	//gmt.run();
+	GeoMap gm = GeoMap();
 	Grandeur tabgrande[4];
 	Capteur cap[98];
 	Mesure mes[1000];
@@ -30,6 +32,25 @@ int main(void) {
 	FluxLog me = FluxLog("mes.csv");
 	while(!me.eof() && i<1000){
 		mes[i]=me.lireDonneesMesure(mes[i],cap,tabgrande,98,4);
+		gm.insert(mes[i]);
 		i++;
 	}
+	Coordonnees centre = Coordonnees(40.0, 0.0);
+
+	time_t dateDebut;
+	time_t dateFin;
+
+	string deb = "2019-01-01 12:00:00";
+	string fin = "2019-12-30 12:00:00";
+
+	struct tm tm;
+	strptime(deb.c_str(),"%Y-%m-%d %H:%M:%S",&tm);
+	dateDebut=mktime(&tm);
+
+	strptime(fin.c_str(),"%Y-%m-%d %H:%M:%S",&tm);
+	dateFin=mktime(&tm);
+
+	resultat res = qualiteMoyenne(gm, centre, 2.0, dateDebut, dateFin);
+
+	cout << res.NO2.ET;
 }
